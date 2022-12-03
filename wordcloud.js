26b1c87c184d1e51c51f1048f8ce3a78b505af96
Onur Alp Bicer <onuralpbicer@gmail.com>
2022-12-03 17:07:18 +1100

    adjusted weighs of word cloud items

--- wordcloud.js
+++ wordcloud.js
@@ -1,33 +1,31 @@
 const el = document.getElementById('tech-skills-container')
 
-const list = [
-	'HTML',
-	'CSS',
-	'TypeScript',
-	'JavaScript',
-	'React',
-	'Svelte',
-	'Angular',
-	'Java',
-	'C',
-	'Python',
-	'Bash',
-	'DynamoDB',
-	'PostgreSQL',
-	'SQLite',
-	'EC2',
-	'Cognito',
-	'RDS',
-	'Api Gateway',
-	'Lambda',
-	'S3',
-	'Linux',
+const data = [
+	{ name: 'HTML', weight: 5 },
+	{ name: 'CSS', weight: 5 },
+	{ name: 'TypeScript', weight: 5 },
+	{ name: 'JavaScript', weight: 5 },
+	{ name: 'React', weight: 5 },
+	{ name: 'Svelte', weight: 4 },
+	{ name: 'Angular', weight: 4 },
+	{ name: 'Java', weight: 3 },
+	{ name: 'C', weight: 2 },
+	{ name: 'Python', weight: 3 },
+	{ name: 'Bash', weight: 4 },
+	{ name: 'DynamoDB', weight: 2 },
+	{ name: 'PostgreSQL', weight: 4 },
+	{ name: 'SQLite', weight: 2 },
+	{ name: 'EC2', weight: 3 },
+	{ name: 'Cognito', weight: 1 },
+	{ name: 'RDS', weight: 2 },
+	{ name: 'Api Gateway', weight: 1 },
+	{ name: 'Lambda', weight: 1 },
+	{ name: 'S3', weight: 1 },
+	{ name: 'Linux', weight: 4 },
+	{ name: 'NodeJS', weight: 5 },
+	{ name: 'Koa', weight: 5 },
+	{ name: 'Express', weight: 2 },
 ]
-
-const data = list.map((word) => ({
-	name: word,
-	weight: 1,
-}))
 
 const chart = Highcharts.chart('tech-skills-container', {
 	accessibility: {
